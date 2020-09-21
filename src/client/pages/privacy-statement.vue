9f918ba9498e61952fd8eef26ce2fb715e6968c7
Anand Graves <anandgraves@users.noreply.github.com>
2020-09-21 14:40:52 +0200

    refactor: add privacy statement content

--- src/client/pages/privacy-statement.vue
+++ src/client/pages/privacy-statement.vue
@@ -1,7 +1,233 @@
 <template>
   <article class="center-content">
-    <h1 class="heading-large">Payment Methods</h1>
-    <div class="body rich-text"></div>
+    <h1 class="heading-large">Payment Statement of 21 Bits</h1>
+    <div class="body rich-text">
+      <p>1 October 2020</p>
+
+      <p class="body">
+        <a
+          href="https://21bits.nl"
+          rel="noopener noreferrer"
+          target="_blank"
+          title="View more information about 21 Bits at 21bits.nl"
+          aria-label="View more information about 21 Bits at 21bits.nl"
+          >21 Bits</a
+        >
+        develops and manages paulblanca.nl, manages all payments, shipping and
+        customer support in partnership with Paul Blanca.
+      </p>
+
+      <p>
+        During the processing of personal data 21 Bits works conform the
+        requirements of the applicable data protection legislation, like the
+        General Data Protection Regulation. This means we:
+      </p>
+
+      <ul>
+        <li>
+          <strong>clearly specify our purposes</strong> before we process
+          personal data, by using this privacy statement;
+        </li>
+        <li>
+          <strong>limit our collection of personal data</strong> to only the
+          personal data needed for legitimate purposes;
+        </li>
+        <li>
+          first <strong>ask for explicit permission</strong> to process your
+          personal data in cases where your permission is required;
+        </li>
+        <li>
+          take <strong>appropriate security measures</strong> to protect your
+          personal data and we demand the same from parties who process personal
+          data on our behalf;
+        </li>
+        <li>
+          <strong>respect</strong> your right to access, correct or delete your
+          personal data held by us.
+        </li>
+      </ul>
+
+      <p>
+        21 Bits is the party responsible for all data processing. In this
+        privacy statement, we will explain what kind of personal data we collect
+        and for which purposes within our web shop https://paulblanca.nl. We
+        recommend that you read it carefully.
+      </p>
+
+      <p>
+        If you have any questions regarding the processing of personal data, you
+        can find the contactdetails of 21 Bits at the end of this privacy
+        statement.
+      </p>
+
+      <h2 class="heading-medium">Handling your order</h2>
+      <p>
+        We will use your personal data when handling your order. We may share
+        your data with the delivery service to have your order delivered. We may
+        also obtain information on your payment from your bank or credit card
+        company.
+      </p>
+
+      <p>
+        For this purpose, we use your name and address details, phone number,
+        invoice address, email address and payment details. We need this data
+        because of our agreement with you. We store this information until your
+        order has been completed. Certain types of personal data will be
+        retained for a longer period with regards to the legal tax retention.
+        (the legal retention period).
+      </p>
+
+      <h2 class="heading-medium">Sending newsletters</h2>
+      <p>
+        We have a newsletter to inform those interested on news, tips and
+        information about our products and services. Your e-mail address is
+        added to the list of subscribers, only with your permission.
+      </p>
+
+      <p>
+        For this purpose, we use your name and address details and email
+        address. We do this on the basis of your consent. We store this
+        information until you cancel your subscription.
+      </p>
+
+      <p>
+        You may cancel your subscription to our newsletter at any time. Each
+        newsletter contains a link to unsubscribe from our newsletter.
+      </p>
+
+      <h2 class="heading-medium">Promotion</h2>
+      <p>
+        Other than the advertisements on the website, we can inform you about
+        new products or services:
+      </p>
+
+      <ul>
+        <li>by e-mail</li>
+        <li>via social media</li>
+      </ul>
+
+      <p>
+        You can object at all times against this promotional communication.
+        Every e-mail contains a cancellation link. On social media, you can
+        block us or use the cancellation option.
+      </p>
+
+      <h2 class="heading-medium">Location data</h2>
+      <p>
+        If necessary, we may collect your location data (GPS). If that is the
+        case, you will be asked to grant consent beforehand.
+      </p>
+
+      <p>
+        This location data and other data can also be stored and processed by
+        the provider of the navigation/mapping software, such as Google Maps,
+        but the data could also be used by, for example, Google or Apple itself.
+        We have no control over their actions. We recommend thatyou read the
+        applicable privacy statement of the provider in question.
+      </p>
+
+      <h2 class="heading-medium">Providing data to third parties</h2>
+      <p>
+        Except for the parties necessary to deliver the above mentioned
+        services, we do not under any circumstance provide your personal data to
+        other companies or organisations, unless we are required to do so by law
+        (for example, when the police demands access to personal data in case of
+        a suspected crime).
+      </p>
+
+      <h2 class="heading-medium">Statistics</h2>
+      <p>
+        We keep statistics on the use of our web shop, but these are kept
+        anonymous at all times.
+      </p>
+
+      <h2 class="heading-me">Security</h2>
+      <p>
+        We take security measures to reduce misuse of and unauthorized access to
+        personal data. We take responsibility in the security of your personal
+        data. We renew our security measures to ensure safe storage of personal
+        data and keep track what might go wrong.
+      </p>
+
+      <h2 class="heading-medium">Changes to this privacy statement</h2>
+      <p>
+        We reserve the right to modify this statement. We recommend that you
+        consult this statement on a regular basis, so that you remain informed
+        of any changes.
+      </p>
+
+      <h2 class="heading-medium">Inspection and modification of your data</h2>
+      <p>
+        You can always contact us if you have any questions regarding our
+        privacy policy or wish to review, modify or delete your personal data.
+      </p>
+
+      <p>You have the following rights:</p>
+
+      <ul>
+        <li>
+          Right of access: you have the right to see what kind of personal data
+          we processed about you;
+        </li>
+        <li>
+          Right of rectification: you have the right to rectify any personal
+          data we have processed about you, if this information is (partially)
+          wrong;
+        </li>
+        <li>
+          Right to complain: you have the right to file a complaint against the
+          processing of your personal data by us, or against direct marketing;
+        </li>
+        <li>
+          Right to be forgotten: you can file a request with us to remove any
+          personal data we have processed of you;
+        </li>
+        <li>
+          Right to data portability: if technically possible, you have the right
+          to ask us to transfer your processed personal data to a third party;
+        </li>
+        <li>
+          Right to restriction of processing: you can file a request with us to
+          (temporarily) restrict the processing of your personal data.
+        </li>
+      </ul>
+
+      <p>
+        If you exercise any of the rights mentioned above, we might ask to
+        identify yourself with a valid ID, to confirm it is your personal data.
+        If so, it is important that you hide your social security number and
+        photo.
+      </p>
+
+      <p>
+        We will usually respond to your request within 30 days. This term can be
+        extended if the request is proven to be complex or tied to a specific
+        right. You will be notified about a possible extension of this term.
+      </p>
+
+      <h2 class="heading-medium">Complaints</h2>
+      <p>
+        If you want to file a complaint about our use of personal data, please
+        send an email with the details of your complaint to
+        <a href="mailto:info@paulblanca.nl">info@paulblanca.nl</a>. We will look
+        into and respond to any complaint we receive.
+      </p>
+
+      <p>
+        If you think that we are not helping you in the right way, you have the
+        right to file a complaint at the authority. For The Netherlands, this is
+        the
+        <a href="https://autoriteitpersoonsgegevens.nl/"
+          >Autoriteit Persoonsgegevens</a
+        >.
+      </p>
+
+      <h2 class="heading-medium">Contact Details</h2>
+      21 Bits<br />
+      Keurenplein 41, Unit E6060<br />
+      1069CD Amsterdam<br />
+      <a href="mailto:info@paulblanca.nl">info@paulblanca.nl</a>
+    </div>
   </article>
 </template>
 
