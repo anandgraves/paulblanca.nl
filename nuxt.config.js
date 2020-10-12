00d2ee9fb765d6f1e344e8549d7889f84594269e
Anand Graves <anandgraves@users.noreply.github.com>
2020-10-12 10:54:48 +0200

    refactor: add android favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,6 +77,18 @@ export default {
     },
     icon: {
       source: `src/client/static/favicons/icon.png?v=${urlHash}`,
+      icons: [
+        {
+          src: '/favicons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/favicons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
     },
   },
   components: true,
