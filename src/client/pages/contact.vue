95b214bc3aa42f9b2f4cae51d988917e985e06a3
Anand Graves <anandgraves@users.noreply.github.com>
2020-11-03 11:53:57 +0100

    refactor: test cloudinary image

--- src/client/pages/contact.vue
+++ src/client/pages/contact.vue
@@ -1,8 +1,7 @@
 <template>
   <article class="center-content rich-text">
     <h1 class="heading-large">Contact</h1>
-    <p>{{ width }}</p>
-    <p>cloudinary image <img :src="src" alt="" /></p>
+    <img :src="src" loading="lazy" alt="test alt tag for image" />
     <p class="body">
       You can contact us at
       <a href="mailto:info@paulblanca.nl">info@paulblanca.nl</a>.
@@ -39,23 +38,16 @@
 
 <script>
 export default {
-  data() {
-    return {
-      width: undefined,
-      src: this.$cloudinary.image.url(
-        'paulblanca/par-la-pluie-des-femmes-1990-ellen-crying-1500w_enqlck.jpg',
+  computed: {
+    src() {
+      return this.$cloudinary.image.url(
+        'paulblanca/par-la-pluie-des-femmes-1990-ellen-crying-1500w_enqlck',
         {
-          fetchFormat: 'auto',
-          responsive: true,
+          width: 'auto',
+          crop: 'scale',
         }
-      ),
-    }
-  },
-  mounted() {
-    const pixelRatio = window.devicePixelRatio || 1
-    const cssWidth = this.$el.getBoundingClientRect().width
-    const width = Math.ceil((cssWidth * pixelRatio) / 100) * 100
-    this.width = Math.min(width, 1378)
+      )
+    },
   },
 }
 </script>
