12d3512590c08837e845b5b276f68cea9568f0d1
Anand Graves <anandgraves@users.noreply.github.com>
2020-11-05 16:31:27 +0100

    refactor(responsive-image): broken image styling

--- src/client/components/responsive-image/responsive-image.vue
+++ src/client/components/responsive-image/responsive-image.vue
@@ -2,16 +2,16 @@
   <figure class="responsive-image">
     <fixed-ratio
       :style="{ backgroundColor: placeholderColor }"
-      :width="1000"
-      :height="1000"
+      :width="1"
+      :height="1"
     >
       <transition name="fade">
         <cld-image
           :cloud-name="cloudName"
-          :public-id="image.id"
+          public-id="1"
           loading="lazy"
           responsive
-          alt="test alt tag for responsive image"
+          :alt="image.alt"
           class="responsive-image__img"
         />
       </transition>
@@ -20,13 +20,12 @@
 </template>
 
 <script>
+/**
+ * This component is modified from https://github.com/voorhoede/vue-dato-image/blob/master/src/vue-dato-image.vue
+ */
 import { CldImage } from 'cloudinary-vue'
 
 export default {
-  /**
-   * This component is modified from https://github.com/voorhoede/vue-dato-image/blob/master/src/vue-dato-image.vue
-   */
-
   components: {
     CldImage,
   },
@@ -51,17 +50,45 @@ export default {
       return this.image.alt || 'Photo by artist and photographer Paul Blanca'
     },
   },
-
-  methods: {
-    onLoad() {
-      this.isLoaded = true
-    },
-  },
 }
 </script>
 
 <style>
 .responsive-image__img {
   width: 100%;
+  height: 100%;
+}
+
+.responsive-image__img img {
+  display: block;
+  position: relative;
+  height: 100%;
+  font-family: 'Helvetica';
+  font-weight: 300;
+  line-height: 2;
+  text-align: center;
+}
+
+.responsive-image__img img:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  display: block;
+}
+
+.responsive-image__img img:after {
+  content: attr(alt);
+  position: absolute;
+  z-index: 1;
+  display: block;
+  width: 300px;
+  left: 50%;
+  transform: translateX(-50%);
+  color: white;
+  font-size: 1.2rem;
 }
 </style>
