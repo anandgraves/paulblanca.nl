1b980a48ca81fee25d6fe0b18f98fa9a01b1ada2
Anand Graves <anandgraves@users.noreply.github.com>
2020-12-02 22:59:44 +0100

    feat: add og:description , og:image to /shop

--- src/client/pages/shop/index.vue
+++ src/client/pages/shop/index.vue
@@ -20,6 +20,23 @@ export default {
           content:
             'Framed, signed and limited prints only. Available in 30x40, 40x50, 100x120cm on Dibond, Plexiglass and TruLife (museum) Plexiglass.',
         },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'Framed, signed and limited prints only. Available in 30x40, 40x50, 100x120cm on Dibond, Plexiglass and TruLife (museum) Plexiglass.',
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: this.$cloudinary.image.url(
+            'portrait-paul-blanca-hans-de-kort_siz4nr',
+            {
+              crop: 'scale',
+              width: 600,
+            }
+          ),
+        },
       ],
       ...setCanonicalForSeo(this.$route),
     }
