9700e778597a835dddf9defc9b41b1f2a2a2325a
Anand Graves <anand@firm24.com>
2022-03-13 09:53:51 +0100

    refactor: add contact email

--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="content">
-    <p>Paul Blanca, artist and photographer, has passed away at the age of 62 on October 14, 2021.</p>
+    <p>Paul Blanca, artist and photographer, passed away at the age of 62 on October 14, 2021.</p>
     <figure>
       <img src="~/assets/paul-blanca-1958-2021.jpg" alt="Portrait of Paul Blanca by Koos Breukel">
       <figcaption>Photo by Koos Breukel</figcaption>
     </figure>
+
+    <p class="contact-details"><a href="mailto:info@paulblanca.nl">info@paulblanca.nl</a></p>
   </div>
 </template>
 
@@ -16,6 +18,15 @@ export default {
 
 <style>
 @import '../components/app-core/settings.css';
+.contact-details {
+  text-decoration: underline;
+  text-align: center;
+}
+
+.contact-details a {
+  color: black;
+}
+
 p {
   font-size: 1.1rem;
   margin-top: 2rem;
